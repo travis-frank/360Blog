2e28ccee086b25397b2e3e247218480f3a5165ef
Jaxson Kahl <jaxsonkahl@gmail.com>
2025-04-07 15:13:16 -0700

    Added search bar functionality

--- src/frontend/userProfile.php
+++ src/frontend/userProfile.php
@@ -93,9 +93,10 @@ if (isset($_SESSION['user_id']) && $_SESSION['user_id'] != $user_id) {
             <?php if (isset($_SESSION['user_id'])): ?>
                 <span class="navbar-text text-white me-3">Welcome, <?= htmlspecialchars($_SESSION['name']) ?></span>
             <?php endif; ?>
-            <form class="d-flex">
-                <input type="text" class="form-control search-bar" placeholder="Search...">
+            <form class="d-flex" action="searchResults.php" method="GET">
+                <input type="text" class="form-control search-bar" name="query" placeholder="Search..." required>
             </form>
+
         </div>
     </nav>
 
